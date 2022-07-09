b32cf4652fae69a53c4e71772fbe04a7ccf1d40e
“tian231825” <2282550468@QQ.COM>
2022-07-09 11:57:32 +0800

    Initial commit

---
+++ GrC.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : GrC.py
+@Time    : 2022/7/9 11:17
+@Author  : junruitian
+@Software: PyCharm
+"""
+
+class GranularComputing(self,):
+    def __init__(self):
+        pass
+
+    def result(self):
+        pass
+
+    # 初始化模糊矩阵 U
+    def initialize_matrix_U(self):
+
+
+
